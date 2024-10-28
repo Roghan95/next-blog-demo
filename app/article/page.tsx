0619e43eb40340742522103c1cac2ae32493587d
Roghan95 <p90svborz@gmail.com>
2024-10-28 09:43:03 +0100

    Project Setup NextJs

---
+++ app/article/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold uppercase mb-5'>Ma première page Next Js</h1>
+      <p className='text-[30px]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore voluptates, ex ipsa odit excepturi nostrum eligendi modi optio doloribus debitis nulla provident repellendus voluptatibus neque explicabo, tempore pariatur iusto quo?</p>
+    </div>
+  )
+}
+
+export default page
