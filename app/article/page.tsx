b4b4fd11ad28f7630bf185291f96927d4b726f6c
Roghan95 <p90svborz@gmail.com>
2024-11-13 07:52:04 +0100

    Added Tailwind properties for a more aesthetic and harmonious display

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -35,28 +35,32 @@ const ArticlePage = () => {
 
   return (
     <>
-      <h1 className='text-2xl font-bold uppercase mb-5'>Blog</h1>
-      {/* Liste des articles */}
-      {articles.map((article: any) => ( // On boucle sur les articles avec la méthode map
-        <div className='mb-6' key={article.id}> {/* On utilise la propriété id de l'article comme clé */}
+      <h1 className='text-4xl font-bold mb-6'>Blog</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {/* Liste des articles */}
+        {articles.map((article: any) => ( // On boucle sur les articles avec la méthode map
+          <div className='group border border-slate-500 p-6 rounded-md hover:bg-slate-700 cursor-pointer hover:-translate-y-2 duration-300' key={article.id}> {/* On utilise la propriété id de l'article comme clé */}
 
-            {/* Titre de l'article */}
-            <h2 className='text-2xl font-semibold text-emerald-700'>{article.title}</h2>
+              {/* Titre de l'article */}
+              <h2 className='text-2xl md:text-xl font-bold'>{article.title}</h2>
 
-            {/* Date / Heure */}
-            <p>{formatDate(article.createdAt)}</p> {/* On formate la date avec la fonction formatDate du dossier lib/utils.ts */}
+              {/* Date / Heure */}
+              <p className='text-sm text-slate-300'>{formatDate(article.createdAt)}</p> {/* On formate la date avec la fonction formatDate du dossier lib/utils.ts */}
 
-            {/* Liste des tags */}
-            {article.tags.map((tagArticle: any) => ( // On boucle sur les tags de l'article avec la méthode map
-              <span key={tagArticle.tag.id}> {/* On utilise la propriété id du tag comme clé */}
-                {tagArticle.tag.name}
-              </span>
-            ))}
+              {/* Liste des tags */}
+              <div className='flex flex-wrap gap-2 my-4'>
+                {article.tags.map((tagArticle: any) => ( // On boucle sur les tags de l'article avec la méthode map
+                  <span className='duration-300 group-hover:bg-pink-600 px-3 py-2 text-xs rounded-full bg-slate-600' key={tagArticle.tag.id}> {/* On utilise la propriété id du tag comme clé */}
+                    {tagArticle.tag.name}
+                  </span>
+                ))}
+              </div>
 
-            {/* Texte de l'article */}
-            <p>{article.text}</p>
-        </div>
-      ))}
+              {/* Texte de l'article */}
+              <p className='line-clamp-4'>{article.text}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
