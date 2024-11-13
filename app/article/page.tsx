4af48f9dcbed78bbeae82acbf79382d9f5d04ffc
Roghan95 <p90svborz@gmail.com>
2024-11-13 06:55:11 +0100

    Ajout de commentaires

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { formatDate } from '@/lib/utils';
+import { formatDate } from '@/lib/utils'; // On importe la fonction formatDate du dossier lib/utils.ts
 import React, { useState, useEffect } from 'react';
 
 const ArticlePage = () => {
@@ -21,33 +21,34 @@ const ArticlePage = () => {
   // });
 
   // VERSION 2 : Hooks
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const response = await fetch('/api/article');
-      const data = await response.json();
-      setArticles(data);
+
+  const [articles, setArticles] = useState([]); // Initialisation du state
+  useEffect(() => { // Hook d'effet pour appeler l'API
+    const fetchArticles = async () => { // Fonction asynchrone
+      const response = await fetch('/api/article'); // On appelle l'API en utilisant la route /api/article avec fetch
+      const data = await response.json(); // On récupère les données de la réponse en JSON
+      setArticles(data); // On met à jour le state avec les données récupérées
     }
 
-    fetchArticles();
-  }, [])
+    fetchArticles(); // On appelle la fonction fetchArticles pour récupérer les données
+  }, []) // Tableau de dépendances vide pour appeler fetchArticles une seule fois
 
   return (
     <>
       <h1 className='text-2xl font-bold uppercase mb-5'>Blog</h1>
       {/* Liste des articles */}
-      {articles.map((article: any) => (
-        <div className='mb-6' key={article.id}>
+      {articles.map((article: any) => ( // On boucle sur les articles avec la méthode map
+        <div className='mb-6' key={article.id}> {/* On utilise la propriété id de l'article comme clé */}
 
             {/* Titre de l'article */}
             <h2 className='text-2xl font-semibold text-emerald-700'>{article.title}</h2>
 
             {/* Date / Heure */}
-            <p>{formatDate(article.createdAt)}</p>
+            <p>{formatDate(article.createdAt)}</p> {/* On formate la date avec la fonction formatDate du dossier lib/utils.ts */}
 
             {/* Liste des tags */}
-            {article.tags.map((tagArticle: any) => (
-              <span key={tagArticle.tag.id}>
+            {article.tags.map((tagArticle: any) => ( // On boucle sur les tags de l'article avec la méthode map
+              <span key={tagArticle.tag.id}> {/* On utilise la propriété id du tag comme clé */}
                 {tagArticle.tag.name}
               </span>
             ))}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 
+// Fonction permettant de formater une date au format anglophone mois, jour, année et heure.
 export function formatDate(date: Date): string {
     return format(new Date(date), "MMMM do, yyyy HH:mm") ?? "Date not available";
 }
